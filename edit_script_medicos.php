38a19e3434c8cf109dc2e9d3f89b597b9c99b838
britoviski <98991610+britoviski@users.noreply.github.com>
2025-08-05 07:58:32 -0300

    edit medicos

---
+++ edit_script_medicos.php
@@ -0,0 +1,55 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Alteração de Cadastro</title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <?php
+            include "conexao.php";
+            $id = $_POST['id'];
+            $crm = $_POST['crm'];
+            $nome = $_POST['nome'];
+            $endereco = $_POST['endereco'];
+            $telefone = $_POST['telefone'];
+            $email = $_POST['email'];
+            $data_nascimento = $_POST['data_nascimento'];
+
+           // $sql = "INSERT INTO pessoas(nome, endereco, telefone, email, data_nascimento) VALUES 
+            //('$nome','$endereco','$telefone','$email','$data_nascimento')";
+
+            $sql = "UPDATE medicos SET crm = '$crm', nome = '$nome', endereco = '$endereco', telefone = '$telefone', email = '$email', data_nascimento = '$data_nascimento' WHERE cod_pessoa = $id";
+
+            if (mysqli_query($conn, $sql)) {
+                mensagem ("$nome Alterado com sucesso!", 'success');
+            } else
+                mensagem ("$nome Não alterado!", 'danger');
+            
+            ?>
+            <a href="index.php" class="btn btn-primary">Voltar</a>
+        </div>
+    </div>
+
+    
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
